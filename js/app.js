3561dd4e97fbfb6bb209907d6189a1ed74076ca9
Oli Thissen <oli@tonick.net>
2022-04-07 00:11:37 +0200

    Input, CSS and performance 🚀

--- js/app.js
+++ js/app.js
@@ -1,48 +1,80 @@
-import { data } from "./config.js";
-
-let birthday = data.birthday;
+import { data as defaultData } from "./config.js";
 
 dayjs.extend(dayjs_plugin_isoWeek);
 dayjs.extend(dayjs_plugin_isSameOrBefore);
 
-let canvas = $("<div/>");
+let data = "";
 
-for (let i = 0; i < data.numberOfYears; i++) {
-  const currentYear = dayjs(birthday).add(i, "years").startOf("isoWeek");
-  const nextYear = dayjs(birthday)
-    .add(i + 1, "years")
-    .startOf("isoWeek");
+function refresh() {
+  let birthday = data.birthday;
+  let years = "";
 
-  let current = currentYear;
+  for (let i = 0; i < data.numberOfYears; i++) {
+    const currentYear = dayjs(birthday).add(i, "years").startOf("isoWeek");
+    const nextYear = dayjs(birthday)
+      .add(i + 1, "years")
+      .startOf("isoWeek");
 
-  let year = $("<div/>").addClass("year");
-  while (current.isBefore(nextYear)) {
-    let week = $("<span/>")
-      .attr("id", current.format("YYYY-MM-DD"))
-      .attr("title", current.format("YYYY-MM-DD"))
-      .html("◻")
-    year.append(week);
+    let current = currentYear;
 
-    if (current.isSameOrBefore(dayjs(), "isoWeek")) {
-      week.text("◼");
+    let weeks = "";
+    while (current.isBefore(nextYear)) {
+      let currentFormatted = current.format("YYYY-MM-DD");
+      let symbol = current.isSameOrBefore(dayjs(), "isoWeek") ? "◼" : "◻";
+      let week = `<span id=${currentFormatted} title=${currentFormatted}>${symbol}</span>`;
+      weeks += week;
+      current = current.add(1, "week");
     }
-    current = current.add(1, "week");
+    let year = `<div class='year'>${weeks}</div>`;
+    years += year;
   }
 
-  $(canvas).append(year);
-  year = $("<div/>").addClass("year");
+  $("#cal").html(years);
+
+  let currentWeek = $("#" + dayjs().startOf("isoWeek").format("YYYY-MM-DD"));
+  currentWeek.addClass("currentWeek");
+
+  if ("specialDates" in data) {
+    data.specialDates.forEach((date) => {
+      let element = $(
+        "#" + dayjs(date[0]).startOf("isoWeek").format("YYYY-MM-DD")
+      );
+      let currentTitle = element.attr("title");
+      element
+        .css("color", date[2])
+        .attr("title", currentTitle + "\n\t" + date[0] + ": " + date[1]);
+    });
+  }
 }
 
-$("#cal").append(canvas);
+loadFromLocalStorage();
+refresh();
 
-let currentWeek = $("#" + dayjs().startOf("isoWeek").format("YYYY-MM-DD"));
-currentWeek.addClass("currentWeek");
+$("#refresh").click(updateAndRefresh());
+$("#input_dob").change(updateAndRefresh());
+$("#input_years").change(updateAndRefresh());
 
-data.specialDates.forEach((date) => {
-  console.log(date);
-  let element = $("#" + dayjs(date[0]).startOf("isoWeek").format("YYYY-MM-DD"));
-  let currentTitle = element.attr("title");
-  element
-    .css("color", date[2])
-    .attr("title", currentTitle + "\n\t" + date[0] + ": " + date[1]);
-});
+$("#save").click(saveToLocalStorage);
+$("#load").click(loadFromLocalStorage);
+
+function updateAndRefresh() {
+  return function () {
+    data.birthday = $("#input_dob").val();
+    data.numberOfYears = $("#input_years").val();
+    refresh();
+  };
+}
+
+function loadFromLocalStorage() {
+  data = JSON.parse(localStorage.getItem("MementoMori"));
+  if (data == null) {
+    data = defaultData;
+  }
+  $("#input_dob").val(data.birthday)
+  $("#input_years").val(data.numberOfYears)
+  refresh();
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("MementoMori", JSON.stringify(data));
+}
